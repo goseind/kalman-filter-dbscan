ecf475c0dfbc5e6c13625f3c1380ba6b256fa98f
otiofrui <spam@dev.otiofrui.org>
2021-10-17 17:06:09 +0200

    adding file

---
+++ Installation_Check.py
@@ -0,0 +1,9 @@
+import scipy
+import numpy
+import sklearn
+import matplotlib
+
+print("Scipy version:" + scipy.__version__)
+print("Numpy version:" + numpy.__version__)
+print("Scikit-learn version:" + sklearn.__version__)
+print("Matplotlib version:" + matplotlib.__version__)
