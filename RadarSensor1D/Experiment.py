25916f54778af6cfcf404cee50039cbbe56fba8e
otiofrui <spam@dev.otiofrui.org>
2021-11-11 22:53:22 +0100

    some changes

--- RadarSensor1D/Experiment.py
+++ RadarSensor1D/Experiment.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from DataGenerationRadar1D import GenerateData, rangeAccuracy, velocityAccuracy
+from DataGenerationRadar1D import GenerateData, rangeAccuracy, velocityAccuracy # Import variables too
 from KalmanFilter import KalmanFilter
-from other_func import Q_discrete_white_noise
+from other_func import Q_discrete_white_noise # Import noise function
 
 opt = {
         "initialDistance": 8,
@@ -10,21 +10,10 @@ opt = {
         "movementRange": 1,
         "frequency": 2,
         "SporadicError": 3,
-        "velocity": 3
+        "velocity": 3 # Add velocity
     }
 
 timeAxis, distValues, velValues, truthDistValues, truthVelValues = GenerateData(type="Static", options=opt)
-
-plt.figure()
-plt.plot(timeAxis, distValues)
-plt.plot(timeAxis, velValues)
-plt.plot(timeAxis, truthDistValues)
-plt.plot(timeAxis, truthVelValues)
-plt.xlabel("time in s")
-plt.legend(["Distance", "Velocity", "Truth distance", "Truth velocity"])
-plt.title("Measurement Data of a 1D Radar Sensor")
-plt.grid(True)
-plt.show()
 
 '''
 Aufgabe:
@@ -32,36 +21,47 @@ Aufgabe:
 2. Testen Sie das Kalman-Filter mit verschiedener Objektbewegungsarten.
 '''
 
-# Hier Ihr Kalman-Filter initialisieren
+# Kalman Filter Initialization
 
-# Messwerte
-dt = .1
+## Parameters and values
+dt = .1 #Time step
 R_var = rangeAccuracy
 Q_var = velocityAccuracy
-x = np.array([distValues[0], velValues[0]]).T
-#pred = [x] # Predictions array
-
-# Kovarianz
+x = np.array([[distValues[0], velValues[0]]]).T
 P = np.diag([rangeAccuracy**2, velocityAccuracy**2])
-R = np.array([[R_var]])
-Q = Q_discrete_white_noise(dim=2, dt=dt, var=Q_var)
-
-# Transition
 F = np.array([[1, dt],
               [0,  1]])
 H = np.array([[1., 0.]])
+R = np.array([[R_var]])
+Q = Q_discrete_white_noise(dim=2, dt=dt, var=Q_var)
+pred = [] # Array for predictions
+cov = []
 
-# Kalman Filter
-kFilter = KalmanFilter(x, P, R, F, H, Q)
-pred = []
+## Kalman Filter Instance
+kFilter = KalmanFilter(x, P, F, H, R, Q)
+
+## Data loop to go through all dist/vel values
 for i in range(np.size(timeAxis)):
-    # hier die Daten ins Kalman-Filter eingeben
-    input = np.array([distValues[i], velValues[i]])
+    input = distValues[i] # Vel Value??
     output = kFilter.Step(input)
     pred.append(output)
 
-# Hier das Ergebnis über die Zeit plotten.
-plt.plot(pred)
-# Um wie viel hat sich die Messgenauigkeit verbessert?
-# Wie beeinflussen die Schätzung der Kovarianzmatrix Q und R die Genauigkeit
-# Fügen Sie zufällige Messfehler mit der Parameter "SporadicError" hinzu, wie verhält sich das Kalman Filter?
+## Plot predictions
+plt.figure()
+plt.plot(timeAxis, distValues)
+plt.plot(timeAxis, velValues)
+plt.plot(timeAxis, truthDistValues)
+plt.plot(timeAxis, truthVelValues)
+# plt.plot(timeAxis, pred)
+plt.xlabel("time in s")
+plt.legend(["Distance", "Velocity", "Truth distance", "Truth velocity"])
+plt.title("Measurement Data of a 1D Radar Sensor")
+plt.grid(True)
+plt.show()
+
+'''
+Questions:
+* Um wie viel hat sich die Messgenauigkeit verbessert?
+* Wie beeinflussen die Schätzung der Kovarianzmatrix Q und R die Genauigkeit
+* Fügen Sie zufällige Messfehler mit der Parameter "SporadicError" hinzu, wie verhält sich das Kalman Filter?
+'''
--- RadarSensor1D/KalmanFilter.py
+++ RadarSensor1D/KalmanFilter.py
@@ -1,33 +1,33 @@
 from matplotlib.pyplot import xcorr
 import numpy as np
 from scipy.linalg import inv
-from other_func import Q_discrete_white_noise
+from other_func import Q_discrete_white_noise # Import noise function
+
+## Kalman Filter Class
 
 class KalmanFilter:
-    # Initialisierung von Kalman Filter
-    def __init__(self, x, P, R, F, H, Q):
+
+    # Class Initialization
+    def __init__(self, x, P, F, H, R, Q):
         self.x = x
         self.P = P
-        self.R = R
-        self.F = F
         self.H = H
+        self.F = F
+        self.R = R
         self.Q = Q
 
-    # Diese Funktion nimmt die Messwerten und gibt 
-    # das Ergebnis des Kalman Filters zurück
-    
-    # Bitte hier geeignete Eingabe- und Rückgabeparametern ergänzen
+    # Predict and update function of the Kalman Filter
     def Step(self, z):
-        # Predict
-        x = self.F @ self.x
-        P = self.F @ self.P @ self.F.T + self.Q
+        # Predict values
+        self.x = self.F @ self.x
+        self.P = self.F @ self.P @ self.F.T + self.Q
 
-        # Update
+        # Update values
         S = self.H @ self.P @ self.H.T + self.R
-        K = P @ self.H.T @ inv(S)
-        y = z - self.H @ x
-        x += K @ y
-        P = P - K @ self.H @ P
+        K = self.P @ self.H.T @ inv(S)
+        y = z - self.H @ self.x
+        self.x += K @ y
+        self.P = self.P - K @ self.H @ self.P
 
         # Return value
-        return x
+        return self.x #, self.P
