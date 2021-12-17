76f741e104962427fbcfbe29b383dc6a909a54ad
otiofrui <spam@dev.otiofrui.org>
2021-12-17 17:56:58 +0100

    random data plot

--- RadarSensor3D/DBScan.py
+++ RadarSensor3D/DBScan.py
@@ -1,3 +1,20 @@
-'''
-Implementieren Sie hier den DBScan-Algorithmus
-'''
+from sklearn.datasets import make_moons
+from matplotlib import pyplot as plt
+
+X, y = make_moons(n_samples=500, noise=0.1, random_state=10)
+
+# When the label y is 0, the class is represented with a blue square.
+# When the label y is 1, the class is represented with a green triangle.
+plt.plot(X[:, 0][y==1], X[:, 1][y==1], "bs")
+plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
+
+# X contains two features, x1 and x2
+plt.xlabel(r"$x_1$", fontsize=20)
+plt.ylabel(r"$x_2$", fontsize=20)
+
+# Simplifying the plot by removing the axis scales.
+plt.xticks([])
+plt.yticks([])
+
+# Displaying the plot.
+plt.show()
