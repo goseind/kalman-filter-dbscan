bbbc0585e4c44c175759a06588c30772a495e0d1
otiofrui <spam@dev.otiofrui.org>
2021-12-22 18:30:03 +0100

    pca and dbscan class

---
+++ RadarSensor3D/PCA.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import decomposition
+
+class custom_pca:
+    def analysis():
+        return 0
