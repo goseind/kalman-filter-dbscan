52215e2714834d9f631a0761243ff2b6247a1ab6
otiofrui <spam@dev.otiofrui.org>
2021-12-22 18:30:18 +0100

    dbscan

--- RadarSensor3D/DBScan.py
+++ RadarSensor3D/DBScan.py
@@ -1,16 +1,9 @@
-from sklearn.datasets import make_moons
-from matplotlib import pyplot as plt
+from sklearn.cluster import DBSCAN
 
-# Plot data
-X, y = make_moons(n_samples=500, noise=0.1, random_state=10)
-
-plt.plot(X[:, 0][y==1], X[:, 1][y==1], "bs")
-plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
-
-plt.xlabel(r"$x_1$", fontsize=20)
-plt.ylabel(r"$x_2$", fontsize=20)
-
-plt.show()
-
-# DBSCAN
+class custom_dbscan:
+    def scan(input):
+        model = DBSCAN()
+        model.fit_predict(input)
+        pred = model.fit_predict(input)
+        return pred
 
